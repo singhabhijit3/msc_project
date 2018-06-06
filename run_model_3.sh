59af3473104eca6d96e724881f75659916f1feaf
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-06-06 12:59:36 +0100

    Add files via upload

---
+++ run_model_3.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16000  # memory in Mb
+#SBATCH -o baseline_1_results  # send stdout to baseline_1_results
+#SBATCH -e experiment_errfile_3  # send stderr to experiment_errfile_3
+#SBATCH -t 20:00:00  # time requested in hour:minute:secon
+export CUDA_HOME=/opt/cuda-8.0.44
+
+export CUDNN_HOME=/opt/cuDNN-6.0_8.0
+
+export STUDENT_ID=$(whoami)
+
+export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
+export CPATH=${CUDNN_HOME}/include:$CPATH
+
+export PATH=${CUDA_HOME}/bin:${PATH}
+
+export PYTHON_PATH=$PATH
+
+mkdir -p /disk/scratch/${STUDENT_ID}
+
+export TMPDIR=/disk/scratch/${STUDENT_ID}/
+export TMP=/disk/scratch/${STUDENT_ID}/
+# Activate the relevant virtual environment:
+
+source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
+
+python baseline_1_for_gpu.py
