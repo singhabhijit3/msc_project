929c1f3ecaadd51bd77bef461e20f4d1d45c7002
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-06-11 12:46:12 +0100

    Update run_model_3.sh

--- run_model_3.sh
+++ run_model_3.sh
@@ -3,9 +3,9 @@
 #SBATCH -n 1	  # tasks requested
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16000  # memory in Mb
-#SBATCH -o baseline_1_results  # send stdout to baseline_1_results
+#SBATCH -o zaremba_results_6  # send stdout to zaremba_results_6
 #SBATCH -e experiment_errfile_3  # send stderr to experiment_errfile_3
-#SBATCH -t 20:00:00  # time requested in hour:minute:secon
+#SBATCH -t 04:00:00  # time requested in hour:minute:secon
 export CUDA_HOME=/opt/cuda-8.0.44
 
 export CUDNN_HOME=/opt/cuDNN-6.0_8.0
@@ -30,4 +30,4 @@ export TMP=/disk/scratch/${STUDENT_ID}/
 
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 
-python baseline_1_for_gpu.py
+python baseline_2_zaremba_for_gpu_extra.py
