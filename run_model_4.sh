7ef07da197cbac93b1866abfead7bcff80e95403
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-06-22 12:51:25 +0100

    Update run_model_4.sh

--- run_model_4.sh
+++ run_model_4.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 1	  # tasks requested
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16000  # memory in Mb
-#SBATCH -o zaremba_bs10  # send stdout to zaremba_bs10
+#SBATCH -o zaremba_cl25  # send stdout to zaremba_cl25
 #SBATCH -e experiment_errfile_4  # send stderr to experiment_errfile_4
 #SBATCH -t 04:00:00  # time requested in hour:minute:secon
 export CUDA_HOME=/opt/cuda-8.0.44
