4d2b91361dceef0bde49ddb6250fb0b4598f0ed3
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-06-22 12:48:25 +0100

    Update baseline_2_zaremba_for_gpu.py

--- baseline_2_zaremba_for_gpu.py
+++ baseline_2_zaremba_for_gpu.py
@@ -139,8 +139,8 @@ class KerasBatchGenerator(object):
 # In[10]:
 
 
-num_steps = 35
-batch_size = 5
+num_steps = 15
+batch_size = 20
 train_data_generator = KerasBatchGenerator(train_data, num_steps, batch_size, vocabulary,
                                            skip_step=num_steps)
 valid_data_generator = KerasBatchGenerator(valid_data, num_steps, batch_size, vocabulary,
