072b157d09f99e71d9badb491979ec89edfa44b7
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-06-22 18:27:06 +0100

    Update baseline_2_zaremba_for_gpu_extra.py

--- baseline_2_zaremba_for_gpu_extra.py
+++ baseline_2_zaremba_for_gpu_extra.py
@@ -139,7 +139,7 @@ class KerasBatchGenerator(object):
 # In[10]:
 
 
-num_steps = 55
+num_steps = 35
 batch_size = 20
 train_data_generator = KerasBatchGenerator(train_data, num_steps, batch_size, vocabulary,
                                            skip_step=num_steps)
@@ -154,6 +154,9 @@ hidden_size = 650
 use_dropout=True
 model = Sequential()
 model.add(Embedding(vocabulary, hidden_size, input_length=num_steps))
+if use_dropout:
+    model.add(Dropout(0.5))
+model.add(CuDNNLSTM(hidden_size, return_sequences=True))
 if use_dropout:
     model.add(Dropout(0.5))
 model.add(CuDNNLSTM(hidden_size, return_sequences=True))
