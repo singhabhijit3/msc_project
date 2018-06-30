cbb996f21180352d3e048902992cd55b194f27dd
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-06-30 19:53:11 +0100

    Update run_model_4.sh

--- run_model_4.sh
+++ run_model_4.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=16000  # memory in Mb
 #SBATCH -o zaremba_rs  # send stdout to zaremba_rs
 #SBATCH -e experiment_errfile_4  # send stderr to experiment_errfile_4
-#SBATCH -t 08:00:00  # time requested in hour:minute:secon
+#SBATCH -t 04:00:00  # time requested in hour:minute:secon
 export CUDA_HOME=/opt/cuda-8.0.44
 
 export CUDNN_HOME=/opt/cuDNN-6.0_8.0
