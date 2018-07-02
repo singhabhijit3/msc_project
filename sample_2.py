20e92848b699a6f3b2fa436ebf4830d3309b7a27
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-07-02 13:22:20 +0100

    Update sample_2.py

--- sample_2.py
+++ sample_2.py
@@ -204,7 +204,7 @@ for ijk in combos:
 #checkpointer = ModelCheckpoint(filepath=data_path + '/model-{epoch:02d}.hdf5', verbose=1)
 #earlystopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
 #reduce_lr = ReduceLROnPlateau(factor=0.1, patience=1, verbose=1)
-    num_epochs = 1
+    num_epochs = 3
     if run_opt == 1:
         model.fit_generator(train_data_generator.generate(), len(train_data)//(batch_size[ijk-1]*num_steps[ijk-1]), num_epochs,
                         validation_data=valid_data_generator.generate(),
