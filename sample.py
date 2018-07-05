f7cb3731dcb02a6ef338755164f82c481f97ef43
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-07-05 10:33:29 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -139,8 +139,8 @@ class KerasBatchGenerator(object):
 # In[10]:
 
 
-num_steps = 16
-batch_size = 11
+num_steps = 31
+batch_size = 17
 train_data_generator = KerasBatchGenerator(train_data, num_steps, batch_size, vocabulary,
                                            skip_step=num_steps)
 valid_data_generator = KerasBatchGenerator(valid_data, num_steps, batch_size, vocabulary,
@@ -150,18 +150,18 @@ valid_data_generator = KerasBatchGenerator(valid_data, num_steps, batch_size, vo
 # In[11]:
 
 
-hidden_size = 600
+hidden_size = 1800
 use_dropout=True
 model = Sequential()
 model.add(Embedding(vocabulary, hidden_size, input_length=num_steps))
 if use_dropout:
-    model.add(Dropout(0.48))
+    model.add(Dropout(0.36))
 model.add(CuDNNLSTM(hidden_size, return_sequences=True))
 if use_dropout:
-    model.add(Dropout(0.48))
+    model.add(Dropout(0.36))
 model.add(CuDNNLSTM(hidden_size, return_sequences=True))
 if use_dropout:
-    model.add(Dropout(0.48))
+    model.add(Dropout(0.36))
 model.add(TimeDistributed(Dense(vocabulary)))
 model.add(Activation('softmax'))
 
