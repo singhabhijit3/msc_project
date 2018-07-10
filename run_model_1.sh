346e8d38f6b7849c59b72b6caf514214596091a6
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-07-10 16:23:07 +0100

    Update run_model_1.sh

--- run_model_1.sh
+++ run_model_1.sh
@@ -3,9 +3,9 @@
 #SBATCH -n 1	  # tasks requested
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16000  # memory in Mb
-#SBATCH -o baseline_1_cl45 # send stdout to baseline_1_cl45
+#SBATCH -o mos_1_trial # send stdout to mos_1_trial
 #SBATCH -e experiment_errfile_1  # send stderr to experiment_errfile_1
-#SBATCH -t 04:00:00  # time requested in hour:minute:secon
+#SBATCH -t 08:00:00  # time requested in hour:minute:secon
 export CUDA_HOME=/opt/cuda-8.0.44
 
 export CUDNN_HOME=/opt/cuDNN-6.0_8.0
@@ -30,4 +30,4 @@ export TMP=/disk/scratch/${STUDENT_ID}/
 
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 
-python baseline_1_for_gpu.py
+python mos_model_1.py
