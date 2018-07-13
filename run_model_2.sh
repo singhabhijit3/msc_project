aaf13100abf0e4b24f2508fd505f9eb40ebb2a09
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-07-13 19:06:36 +0100

    Update run_model_2.sh

--- run_model_2.sh
+++ run_model_2.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-#SBATCH -p LongJobs
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16000  # memory in Mb
-#SBATCH -o mos_trial_1  # send stdout to mos_trial_1
+#SBATCH -o mos_sgd2  # send stdout to mos_sgd2
 #SBATCH -e experiment_errfile_2  # send stderr to experiment_errfile_2
-#SBATCH -t 70:00:00  # time requested in hour:minute:secon
+#SBATCH -t 08:00:00  # time requested in hour:minute:secon
 export CUDA_HOME=/opt/cuda-8.0.44
 
 export CUDNN_HOME=/opt/cuDNN-6.0_8.0
@@ -31,4 +30,4 @@ export TMP=/disk/scratch/${STUDENT_ID}/
 
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 
-python mos_model_1.py
+python mos_model_2.py
