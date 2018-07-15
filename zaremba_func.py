96d9f36b6390d375f6ce571d4e0400a5953bff5a
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-07-15 12:39:23 +0100

    Update zaremba_func.py

--- zaremba_func.py
+++ zaremba_func.py
@@ -171,7 +171,7 @@ lstm_model = Model(inputs=inp, outputs=output)
 # In[12]:
 
 
-optim = SGD(lr=1.0)
+optim = SGD(lr=100.0)
 lstm_model.compile(loss='categorical_crossentropy', optimizer=optim, metrics=['categorical_accuracy'])
 
 
