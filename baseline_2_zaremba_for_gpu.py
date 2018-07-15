399711184550dc419b74c1148f3d19cbcc8309c8
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-07-15 12:38:57 +0100

    Update baseline_2_zaremba_for_gpu.py

--- baseline_2_zaremba_for_gpu.py
+++ baseline_2_zaremba_for_gpu.py
@@ -171,7 +171,7 @@ lstm_model = Model(inputs=inp, outputs=output)
 # In[12]:
 
 
-optim = SGD()
+optim = SGD(lr=10)
 lstm_model.compile(loss='categorical_crossentropy', optimizer=optim, metrics=['categorical_accuracy'])
 
 
