24d47ec319f7d411bd4b04251fb3fd8912643fc6
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-07-18 11:20:21 +0100

    Update baseline_2_zaremba_for_gpu_extra.py

--- baseline_2_zaremba_for_gpu_extra.py
+++ baseline_2_zaremba_for_gpu_extra.py
@@ -149,7 +149,7 @@ valid_data_generator = KerasBatchGenerator(valid_data, num_steps, batch_size, vo
 # In[11]:
 
 
-hidden_size = 650
+hidden_size = 1300
 use_dropout=True
 
 inp = Input(shape=(num_steps,), dtype='int32')
