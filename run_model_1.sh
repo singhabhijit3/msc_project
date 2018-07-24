65eca950f74c618738ed75abd5705d81a6a2cbcf
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-07-24 17:32:45 +0100

    Update run_model_1.sh

--- run_model_1.sh
+++ run_model_1.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+#SBATCH -p LongJobs
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16000  # memory in Mb
 #SBATCH -o mos_rd # send stdout to mos_rd
 #SBATCH -e experiment_errfile_1  # send stderr to experiment_errfile_1
-#SBATCH -t 08:00:00  # time requested in hour:minute:secon
+#SBATCH -t 70:00:00  # time requested in hour:minute:secon
 export CUDA_HOME=/opt/cuda-8.0.44
 
 export CUDNN_HOME=/opt/cuDNN-6.0_8.0
