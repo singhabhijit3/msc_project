782da999028ce3089bb0c40d404bf0801f066c41
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-07-25 18:55:44 +0100

    Update sample_3.py

--- sample_3.py
+++ sample_3.py
@@ -139,7 +139,7 @@ class KerasBatchGenerator(object):
 
 
 num_steps = 35
-batch_size = 26
+batch_size = 16
 n_experts = 10
 train_data_generator = KerasBatchGenerator(train_data, num_steps, batch_size, vocabulary,
                                            skip_step=num_steps)
@@ -187,7 +187,7 @@ lstm_model = Model(inputs=inp, outputs=model_output)
 # In[12]:
 
 
-optim = SGD(lr=1)
+optim = SGD(lr=2)
 lstm_model.compile(loss='categorical_crossentropy', optimizer=optim, metrics=['categorical_accuracy'])
 
 
