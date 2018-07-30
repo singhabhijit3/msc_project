60c361861094fb0617c1e6e7fea6bd095da6e2da
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-07-30 10:53:02 +0100

    Update run_model_2.sh

--- run_model_2.sh
+++ run_model_2.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 1	  # tasks requested
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16000  # memory in Mb
-#SBATCH -o mos_cl15  # send stdout to mos_cl15
+#SBATCH -o mos_cl30  # send stdout to mos_cl30
 #SBATCH -e experiment_errfile_2  # send stderr to experiment_errfile_2
 #SBATCH -t 08:00:00  # time requested in hour:minute:secon
 export CUDA_HOME=/opt/cuda-8.0.44
