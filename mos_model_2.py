b37ddb56343c53dbcaeef9acf4d0500a4c8a3721
singhabhijit3 <36075714+singhabhijit3@users.noreply.github.com>
2018-07-31 01:20:16 +0100

    Update mos_model_2.py

--- mos_model_2.py
+++ mos_model_2.py
@@ -138,7 +138,7 @@ class KerasBatchGenerator(object):
 # In[10]:
 
 
-num_steps = 45
+num_steps = 60
 batch_size = 5
 n_experts = 10
 train_data_generator = KerasBatchGenerator(train_data, num_steps, batch_size, vocabulary,
